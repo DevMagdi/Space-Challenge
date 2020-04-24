a06b32ffdd557bef4d437b5d69b9f88a0cb7dad6
mohamed magdy <36678529+devmagdyy@users.noreply.github.com>
2020-04-24 16:35:42 +0200

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,13 @@
+/**
+ * This class represents an item to be carried by the rockets.
+ * Created by Mohamed Magdy on 04/24/2020.
+ */
+public class Item {
+    String name;
+    int weight;
+
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+}
